b21d25526ca85d8dc28cd46eba0f15d6f69a3860
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-05 18:49:47 -0700

    added color block feature, allowed users to save block events to local storage and stay on refresh

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,5 +20,77 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+  
+
+function currentTime () {
+    var today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D, YYYY [at] hh:mm:ss a'));
+    }
+
+currentTime();
+setInterval(currentTime, 1000);
+
+function pastPresentFuture() {
+    var presentTime = parseInt(dayjs().format('H')); // use H instead of h for 24-hour format
+
+    for (var i = 9; i < 18; i++) {
+        var timeBlock = document.getElementById("hour-"+ i);
+        
+        if (i < presentTime) {
+            timeBlock.classList.add("past");
+          } else if (i === presentTime) {
+            timeBlock.classList.add("present");
+          } else {
+            timeBlock.classList.add("future");
+          }
+    }
+}
+        
+
+pastPresentFuture();
+
+document.addEventListener("DOMContentLoaded", function() {
+    for (var i = 9; i < 18; i++) {
+      var saveBtn = document.querySelectorAll(".saveBtn")[i - 9];
+      var blockInput = document.querySelector("#block-" + i);
+      var blockValue = JSON.parse(localStorage.getItem("block-" + i)) || "";
+      
+      blockInput.value = blockValue;
+  
+      saveBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        var blockInput = this.previousElementSibling;
+        var blockValue = blockInput.value.trim();
+        var blockIndex = this.getAttribute("id").replace("saveBtn-", "");
+  
+        localStorage.setItem("block-" + blockIndex, JSON.stringify(blockValue));
+      });
+    }
+  });
+  
+  window.addEventListener("load", function() {
+    for (i = 9; i < 18; i++) {
+      var inputText = document.querySelector("#block-" + i);
+      var savedBlock = JSON.parse(localStorage.getItem("block-" + i));
+      if (savedBlock) {
+        inputText.value = savedBlock;
+      }
+    }
+  });
+
+
+
+
+
+    
+  
+  window.addEventListener("load", function() {
+    for (i=9; i< 18; i++) {
+        var inputText = document.getElementById("#block-"+ i)
+        var savedBlock = JSON.parse(localStorage.getItem("#block-"+ i));
+        if (savedBlock) {
+            inputText.value = savedBlock;
+        }
+    }
   });
   
